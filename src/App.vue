421caa262c66554361a588fcc6ff1ca0184b69d8
melifsaad <113357597+melifsaad@users.noreply.github.com>
2022-10-18 12:52:26 +0200

    4

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,12 @@
   <router-view></router-view>
 </template>
 <script setup>
+
+
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_KEY)
+console.log (supabase)
 console.log (process.env.NODE_ENV)
 </script>
 <style scoped>
