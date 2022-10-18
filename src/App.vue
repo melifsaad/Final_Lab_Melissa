8097a7252f2644acb55d722cf8228b33952bec23
melifsaad <113357597+melifsaad@users.noreply.github.com>
2022-10-18 12:11:36 +0200

    3

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <h1>Task</h1>
+  <ul>
+    <li>
+      <router-link :to="{name: 'home'}"> Home </router-link>
+    </li>
+    <li>
+      <router-link :to="{name: 'login'}"> Login </router-link>
+    </li>
+  </ul>
+  <router-view></router-view>
 </template>
 <script setup>
 console.log (process.env.NODE_ENV)
